cc3c96f051e110b48f74a7f2f459d63c95a8e9fe
Hadil-daibes <135008111+Hadil-daibes@users.noreply.github.com>
2023-05-30 16:04:50 +0300

    commit init

--- cypress/e2e/google.feature
+++ cypress/e2e/google.feature
@@ -1,10 +1,15 @@
 Feature: Login
 
-  # I want to Log into Conduit
+  I want to Log into Conduit
 
 
-  Scenario: Conduit Login
-    Given I open the Conduit login page
-    When I type the email and password
-    And I click on the Sign in button
-    Then "Your Feed" should be show
+  # Scenario: Conduit Login
+  #   Given I open the Conduit login page
+  # When I type the email and password
+  # And I click on the Sign in button
+  # Then "Your Feed" should be show
+
+  Scenario: open editer page
+    Given Conduit login page
+    When I Click on the new post
+    Then Open the Edit page
--- cypress/e2e/google/google.js
+++ cypress/e2e/google/google.js
@@ -1,18 +1,29 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given("I open the Conduit login page", () => {
+Given("Conduit login page", () => {
   cy.visit("https://react-redux.realworld.io/#/login");
-});
-
-When("I type the email and password", () => {
-  cy.get('input[type="email"]').type("Hadil+1@userpilot.co")
-  cy.get('input[type="password"]').type("Hadeel99@")
-});
-
-When("I click on the Sign in button", () => {
+  cy.get('input[type="email"]').type("Hadil+1@userpilot.co");
+  cy.get('input[type="password"]').type("Hadeel99@");
   cy.get(".btn").contains("Sign in").click();
+
 });
 
-Then("{string} should be show", (content) => {
-  cy.contains(content, { timeout: 10000 }).should('be.visible');
+// When("I type the email and password", () => {
+//   cy.get('input[type="email"]').type("Hadil+1@userpilot.co")
+//   cy.get('input[type="password"]').type("Hadeel99@")
+// });
+
+// When("I click on the Sign in button", () => {
+//   cy.get(".btn").contains("Sign in").click();
+// });
+
+// Then("{string} should be show", (content) => {
+//   cy.contains(content, { timeout: 10000 }).should('be.visible');
+// });
+
+When("I Click on the new post", () => {
+  cy.contains("New Post").click();
+});
+Then("Open the Edit page", () => {
+  cy.url().should("include", "#/editor");
 });
